73f01bfa15c70c60d6f580086e1a63fa8e050cd2
Akash200307 <tamilthalaivas7@gmail.com>
2023-03-12 18:13:14 +0530

    first commit

---
+++ KM.py
@@ -0,0 +1,4 @@
+kMS = float(input("Enter value in kilometers: "))
+conv_fac = 0.621371
+miles = kMS * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kMS,miles))
---
+++ add1.py
@@ -0,0 +1,4 @@
+num1 = int(input("Please enter Number1...:"))
+num2 = int(input("Please enter Number2...:"))
+sum=num1+num2
+print("The sum of {0} and {1}is{2}7".format(num1,num2,sum))
